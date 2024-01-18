00f674485676e2542e2ece681c57ce8b5c25ab63
Simeon 2004 <156342085+Bucksly2004@users.noreply.github.com>
2024-01-18 09:57:20 -0500

    Add files via upload

---
+++ Interacrtive Shell.py
@@ -0,0 +1,10 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Simeon McLeod")
+Simeon McLeod
+>>> print("COP2002-0T1")
+COP2002-0T1
+>>> print("Winter 2024")
+Winter 2024
+>>> print("This is an interactive session!")
+This is an interactive session!
